699ca372a9be34f8140e79efc1b0bea8c7adc825
benji-35 <benjamin.delvert@epitech.eu>
2021-01-07 22:21:08 +0100

    Add : Summary of check

--- src/display_resume.c
+++ src/display_resume.c
@@ -9,11 +9,11 @@
 
 void display_resume(int nb_inden, int nb_spaces, int verif_space)
 {
-    printf("\n==========================\n");
-    printf("    RESUME INDENTATION\n");
-    printf("==========================\n");
+    printf("\n===========================\n");
+    printf("    SUMMARY INDENTATION\n");
+    printf("===========================\n");
     printf("Indentation : %d\n", nb_inden);
     if (verif_space)
         printf("Spaces at end : %d\n", nb_spaces);
-    printf("==========================\n");
+    printf("===========================\n");
 }
